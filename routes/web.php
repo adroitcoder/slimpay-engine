69b4865dfec3320b0202629e404feaf2ca14232e
adroitcoder <johnsamuel254@yahoo.com>
2021-10-28 13:04:15 +0100

    init

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return [
         "status"=>true,
-        "message"=>"Forever here to serve you"
+        "message"=>"Forever here to serve you."
     ];
 });
